b5d940d428151a983c5ecd5466d0e0f7b970ca2c
Alyssa Balaoro <balaoroalyssa@gmail.com>
2021-12-11 17:53:59 +0800

    Contact me

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,32 @@ let swiper = new Swiper('.portfolio_container', {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
-
+/*==================== CONTACT ME ====================*/
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+      
+      setTimeout(function() {
+        document.querySelector('.success').remove();
+        form.reset();
+    }, 3000);
+    }
+    form.addEventListener("submit", handleSubmit)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
